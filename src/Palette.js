e985f27e40b8f24fa8d65bdabef52fa362d458c9
Glib Kostsov <glibkostsov@gmail.com>
2021-04-20 07:52:13 +0200

    create react app, add colors seed file

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div className='Palette'>
+        {/*navbar goes here*/}
+        <div className='Palette-colors'>{/* color boxes here */}</div>
+        {/* footer goes here */}
+      </div>
+    )
+  }
+}
