9e6e266027b8402b389bcb784120372cf97ffb86
Glib Kostsov <glibkostsov@gmail.com>
2021-04-26 10:58:19 +0200

    connect ColorPicker to addColor  Button

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -75,6 +75,16 @@ const styles = (theme) => ({
 })
 
 class NewPaletteForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: true,
+      currentColor: 'green',
+      colors: ['green', '#e15764'],
+    }
+    this.updateCurrentColor = this.updateCurrentColor.bind(this)
+    this.addNewColor = this.addNewColor.bind(this)
+  }
   state = {
     open: false,
   }
@@ -84,6 +94,14 @@ class NewPaletteForm extends Component {
 
   handleDrawerClose = () => {
     this.setState({ open: false })
+  }
+
+  updateCurrentColor(newColor) {
+    this.setState({ currentColor: newColor.hex })
+  }
+
+  addNewColor() {
+    this.setState({ colors: [...this.state.colors, this.state.currentColor] })
   }
 
   render() {
@@ -142,10 +160,15 @@ class NewPaletteForm extends Component {
             </div>
 
             <ChromePicker
-              color='purple'
-              onChangeComplete={(newColor) => console.log(newColor)}
+              color={this.state.currentColor}
+              onChangeComplete={this.updateCurrentColor}
             />
-            <Button variant='contained' color='primary'>
+            <Button
+              variant='contained'
+              color='primary'
+              style={{ backgroundColor: this.state.currentColor }}
+              onClick={this.addNewColor}
+            >
               Add Color
             </Button>
           </Drawer>
@@ -155,6 +178,12 @@ class NewPaletteForm extends Component {
             })}
           >
             <div className={classes.drawerHeader} />
+
+            <ul>
+              {this.state.colors.map((color) => (
+                <li style={{ backgroundColor: color }}>{color}</li>
+              ))}
+            </ul>
           </main>
         </div>
       </>
