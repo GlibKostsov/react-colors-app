f06001aaf1f514f36d55ede5499c710425cc8e60
Glib Kostsov <glibkostsov@gmail.com>
2021-04-28 17:17:18 +0200

    rectify Styles of NewPaletteForm Component

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -9,8 +9,8 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
 import Button from '@material-ui/core/Button'
+import AddToPhotosIcon from '@material-ui/icons/AddToPhotos'
 //Local Components & Styles
 import PaletteMetaForm from './PaletteMetaForm'
 import styles from './styles/PaletteFormNavStyles'
@@ -55,10 +55,13 @@ class PaletteFormNav extends Component {
               aria-label='open drawer'
               onClick={handleDrawerOpen}
               edge='start'
-              className={classNames(classes.menuButton, open && classes.hide)}
+              className={classNames(classes.menuButton, {
+                [classes.hide]: open,
+              })}
             >
-              <MenuIcon />
+              <AddToPhotosIcon />
             </IconButton>
+
             <Typography variant='h6' noWrap>
               Create A Palette
             </Typography>
