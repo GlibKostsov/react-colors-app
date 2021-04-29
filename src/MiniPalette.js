760fa1d9a57d716265f8b7c326cd8cb354bb85bb
Glib Kostsov <glibkostsov@gmail.com>
2021-04-29 14:42:30 +0200

    add responsive Styles Helper & style ColorBox & Palette Component(s)

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,31 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 //Material UI Styling
 import { withStyles } from '@material-ui/styles'
 import DeleteIcon from '@material-ui/icons/Delete'
 //Local Components & Styles
 import styles from './styles/MiniPaletteStyles'
 
-function MiniPalette(props) {
-  const { classes, paletteName: name, emoji, colors } = props
-  const miniColorBoxes = colors.map((color) => (
-    <div
-      className={classes.miniColor}
-      style={{ backgroundColor: color.color }}
-      key={color.name}
-    ></div>
-  ))
+class MiniPalette extends Component {
+  constructor(props) {
+    super(props)
+    this.deletePalette = this.deletePalette.bind(this)
+  }
+  deletePalette(evt) {
+    evt.stopPropagation()
+    this.props.handleDelete(this.props.id)
+  }
 
-  return (
-    <div className={classes.root} onClick={props.handleClick}>
-      <div className={classes.delete}>
-        <DeleteIcon className={classes.deleteIcon} />
+  render() {
+    const { classes, paletteName, emoji, colors, handleClick } = this.props
+    const miniColorBoxes = colors.map((color) => (
+      <div
+        className={classes.miniColor}
+        style={{ backgroundColor: color.color }}
+        key={color.name}
+      ></div>
+    ))
+    return (
+      <div className={classes.root} onClick={handleClick}>
+        <div className={classes.delete}>
+          <DeleteIcon
+            className={classes.deleteIcon}
+            onClick={this.deletePalette}
+          />
+        </div>
+        <div className={classes.colors}>{miniColorBoxes}</div>
+        <h5 className={classes.title}>
+          {paletteName} <span className={classes.emoji}>{emoji}</span>
+        </h5>
       </div>
-      <div className={classes.colors}>{miniColorBoxes}</div>
-      <h5 className={classes.title}>
-        {name} <span className={classes.emoji}>{emoji}</span>
-      </h5>
-    </div>
-  )
+    )
+  }
 }
 
 export default withStyles(styles)(MiniPalette)
--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -1,4 +1,7 @@
+//Finds Color Lunminance
 import chroma from 'chroma-js'
+//Local Styles
+import sizes from './sizes'
 
 export default {
   ColorBox: {
@@ -11,6 +14,18 @@ export default {
     marginBottom: '-3.5px',
     '&:hover button': {
       opacity: 1,
+    },
+    [sizes.down('lg')]: {
+      width: '25%',
+      height: (props) => (props.showingFullPalette ? '20%' : '33.3333%'),
+    },
+    [sizes.down('md')]: {
+      width: '50%',
+      height: (props) => (props.showingFullPalette ? '10%' : '20%'),
+    },
+    [sizes.down('xs')]: {
+      width: '100%',
+      height: (props) => (props.showingFullPalette ? '5%' : '10%'),
     },
   },
   copyText: {
@@ -108,6 +123,9 @@ export default {
       marginBottom: '0',
       padding: '1rem',
       textTransform: 'uppercase',
+      [sizes.down('xs')]: {
+        fontSize: '5rem',
+      },
     },
     '& p': {
       fontsize: '2rem',
