03d19448d43f9864556c8a40a09c5150e0045ac0
Glib Kostsov <glibkostsov@gmail.com>
2021-04-30 16:16:34 +0200

    refactor MiniPalette to PureComponent

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,22 +1,27 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 //Material UI Styling
 import { withStyles } from '@material-ui/styles'
 import DeleteIcon from '@material-ui/icons/Delete'
 //Local Components & Styles
 import styles from './styles/MiniPaletteStyles'
 
-class MiniPalette extends Component {
+class MiniPalette extends PureComponent {
   constructor(props) {
     super(props)
     this.deletePalette = this.deletePalette.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
   deletePalette(evt) {
     evt.stopPropagation()
     this.props.openDialog(this.props.id)
   }
 
+  handleClick() {
+    this.props.goToPalette(this.props.id)
+  }
+
   render() {
-    const { classes, paletteName, emoji, colors, handleClick } = this.props
+    const { classes, paletteName, emoji, colors } = this.props
     const miniColorBoxes = colors.map((color) => (
       <div
         className={classes.miniColor}
@@ -25,7 +30,7 @@ class MiniPalette extends Component {
       ></div>
     ))
     return (
-      <div className={classes.root} onClick={handleClick}>
+      <div className={classes.root} onClick={this.handleClick}>
         <div className={classes.delete}>
           <DeleteIcon
             className={classes.deleteIcon}
